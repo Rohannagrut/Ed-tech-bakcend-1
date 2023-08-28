140ddc5445ca7ef866d5bf42c1a171794c0ec0ae
Rohannagrut <92144008+Rohannagrut@users.noreply.github.com>
2023-08-28 18:54:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(cookieParser());
 // );
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: `${process.env.BASE_URL}`,
     credentials: true,
   })
 );
